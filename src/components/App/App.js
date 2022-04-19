e4a62ddfb818f7076383422c6e7547bcf3d8e62d
Margarita555 <a_rita@ukr.net>
2022-04-19 22:12:06 +0300

    added files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import AppBar from '../AppBar/AppBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <AppBar />
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +18,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,12 @@
+export default function AppBar() {
+  //   const isLoggedIn = useSelector(getIsLoggedIn);
+  return (
+    <header
+    //   className={s.header}
+    >
+      ffffffffff
+      {/* <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
+    </header>
+  );
+}
